349402e6e98dc851d3df4ff64d9c698e30f341f6
Taufikisme <mohtaufikafandi@gmail.com>
2021-11-24 18:40:44 +0700

    fix: card number size

--- src/components/CardNumber.js
+++ src/components/CardNumber.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function CardNumber({ number }) {
   return (
-    <div className="flex justify-center items-center w-6 h-6 rounded-full bg-purple-600 text-white text-xs">
+    <div
+      className="flex justify-center items-center rounded-full bg-purple-600 text-white text-xs"
+      style={{ width: "26px", height: "24px" }}
+    >
       {number}
     </div>
   );
