675adf7c352e6ca1e3d4bec84c18f680a370323c
Taufikisme <mohtaufikafandi@gmail.com>
2021-11-25 11:05:13 +0700

    fix: make cardNumber totally circle

--- src/components/CardNumber.js
+++ src/components/CardNumber.js
@@ -4,7 +4,7 @@ export default function CardNumber({ number }) {
   return (
     <div
       className="flex justify-center items-center rounded-full bg-purple-600 text-white text-xs"
-      style={{ width: "26px", height: "24px" }}
+      style={{ width: "26px", height: "26px" }}
     >
       {number}
     </div>
